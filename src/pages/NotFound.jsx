59d31deaefd74b05670e060c36d2feb062465919
NY-ANDRY <nyandrypaulferdinah@gmail.com>
2025-03-06 23:11:06 +0300

    statistique

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -1,16 +1,12 @@
 import Matrix from "../components/boxEffect/Matrix";
-import Nav from "../components/layout/Nav";
 
 const NotFound = () => {
-
-    const navigation = [{ label: "home", link: "/home" }];
 
     return (
         <>
             <div id="notFound" className="flex-center w-full h-[100vh] pb-20 text-xl">
                 <Matrix speed={50}>Not Found</Matrix>
             </div>
-            <Nav pages={navigation} show={true} />
         </>
     );
 }
